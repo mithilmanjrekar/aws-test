eae76bb62807d869ca8de34ef1feac41defb4162
Mithil Manjrekar <mithil.manjrekar@cuelogic.com>
2017-12-12 13:13:15 +0530

    Added rspec test triggering for the shell file run from the jenkins ci cd

--- dockerpostgreslink.sh
+++ dockerpostgreslink.sh
@@ -18,29 +18,29 @@ systemctl start docker
 
 docker ps
 
-echo "Create a docker postgres image .........."
+# echo "Create a docker postgres image .........."
 
-docker build -t postgres_image .
+# docker build -t postgres_image .
 
-echo "Run the docker postgres image .........."
+# echo "Run the docker postgres image .........."
 
-docker run --rm -P --name postgres postgres_image
+# docker run --rm -P --name postgres postgres_image
 
-echo "Docker building the rails app form the Dockerfile .........."
+# echo "Docker building the rails app form the Dockerfile .........."
 
-docker build -t rails-app .
+# docker build -t rails-app .
 
-echo "Docker running the rails app container created form the Dockerfile .........."
+# echo "Docker running the rails app container created form the Dockerfile .........."
 
-docker run  --rm -d --name rails-connect-to-potgres --link postgres:postgres -p 3000:3000 rails-app
+# docker run  --rm -d --name rails-connect-to-potgres --link postgres:postgres -p 3000:3000 rails-app
 
-echo "Docker running the migrations on postgres .........."
+# echo "Docker running the migrations on postgres .........."
 
-docker exec rails-connect-to-potgres bundle exec rake db:create
-docker exec rails-connect-to-potgres bundle exec rake db:migrate
+# docker exec rails-connect-to-potgres bundle exec rake db:create
+# docker exec rails-connect-to-potgres bundle exec rake db:migrate
 
-echo "Running the docker test cases .........."
+# echo "Running the docker test cases .........."
 
-docker exec rails-connect-to-potgres rspec spec/models/user_spec.rb 
+# docker exec rails-connect-to-potgres rspec spec/models/user_spec.rb 
 
-echo "Docker ends testing here .........."
+# echo "Docker ends testing here .........."
