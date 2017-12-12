385c1871598c79b91887e0888206ccc52a17956a
Mithil Manjrekar <mithil.manjrekar@cuelogic.com>
2017-12-12 13:10:29 +0530

    Added rspec test triggering for the shell file run from the jenkins ci cd

--- dockerpostgreslink.sh
+++ dockerpostgreslink.sh
@@ -1,41 +1,46 @@
 #!/bin/sh
-# https://docs.docker.com/engine/installation/linux/ubuntu/#install-using-the-repository
-sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88 | grep docker@docker.com || exit 1
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
-sudo docker run --rm hello-world
-service docker retart
+
+sudo apt-get -y update
+
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+
+sudo apt-get -y update
+
+apt-cache policy docker-engine
+
+sudo apt-get -y install -y docker-engine
+
+sudo systemctl status docker
+
+sudo systemctl start docker
 
 docker ps
 
-# echo "Create a docker postgres image .........."
+echo "Create a docker postgres image .........."
 
-# docker build -t postgres_image .
+docker build -t postgres_image .
 
-# echo "Run the docker postgres image .........."
+echo "Run the docker postgres image .........."
 
-# docker run --rm -P --name postgres postgres_image
+docker run --rm -P --name postgres postgres_image
 
-# echo "Docker building the rails app form the Dockerfile .........."
+echo "Docker building the rails app form the Dockerfile .........."
 
-# docker build -t rails-app .
+docker build -t rails-app .
 
-# echo "Docker running the rails app container created form the Dockerfile .........."
+echo "Docker running the rails app container created form the Dockerfile .........."
 
-# docker run  --rm -d --name rails-connect-to-potgres --link postgres:postgres -p 3000:3000 rails-app
+docker run  --rm -d --name rails-connect-to-potgres --link postgres:postgres -p 3000:3000 rails-app
 
-# echo "Docker running the migrations on postgres .........."
+echo "Docker running the migrations on postgres .........."
 
-# docker exec rails-connect-to-potgres bundle exec rake db:create
-# docker exec rails-connect-to-potgres bundle exec rake db:migrate
+docker exec rails-connect-to-potgres bundle exec rake db:create
+docker exec rails-connect-to-potgres bundle exec rake db:migrate
 
-# echo "Running the docker test cases .........."
+echo "Running the docker test cases .........."
 
-# docker exec rails-connect-to-potgres rspec spec/models/user_spec.rb
+docker exec rails-connect-to-potgres rspec spec/models/user_spec.rb 
 
-# curl 
-
-# echo "Docker ends testing here .........."
+echo "Docker ends testing here .........."
