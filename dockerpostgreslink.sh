cd76e5b9d9d712f4d727bce57f241401c73bed0c
Mithil Manjrekar <mithil.manjrekar@cuelogic.com>
2017-12-12 14:37:55 +0530

    Added the docker potgre slink shell file

--- dockerpostgreslink.sh
+++ dockerpostgreslink.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-sudo apt-get -y update
+apt-get -y update
 
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | tee /etc/apt/sources.list.d/docker.list
 
-sudo apt-get -y update
+apt-get -y update
 
 apt-cache policy docker-engine
 
-sudo apt-get -y install -y docker-engine
+apt-get -y install -y docker-engine
 
-sudo systemctl status docker
+systemctl status docker
 
 docker ps
 
