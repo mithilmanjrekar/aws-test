cf3aa0bf3521234120c133f04cf3d7ed98c24c54
Mithil Manjrekar <mithil.manjrekar@cuelogic.com>
2017-12-12 15:27:10 +0530

    Updated the shell file to link docker and potgres

--- dockerpostgreslink.sh
+++ dockerpostgreslink.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
-
-apt-get -y update
-
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | tee /etc/apt/sources.list.d/docker.list
-
-apt-get -y update
-
-apt-cache policy docker-engine
-
-apt-get -y install -y docker-engine
-
-systemctl status docker
+# https://docs.docker.com/engine/installation/linux/ubuntu/#install-using-the-repository
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88 | grep docker@docker.com || exit 1
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo docker run --rm hello-world
+service docker start
 
 docker ps
 
