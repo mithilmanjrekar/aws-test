15b4cbdec03bd6dd635a3d62959197e60f9b6606
Mithil Manjrekar <mithil.manjrekar@cuelogic.com>
2017-12-12 15:45:40 +0530

    Updated the shell file to link docker and potgres

--- dockerpostgreslink.sh
+++ dockerpostgreslink.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # https://docs.docker.com/engine/installation/linux/ubuntu/#install-using-the-repository
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88 | grep docker@docker.com || exit 1
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
-sudo docker run --rm hello-world
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo apt-key fingerprint 0EBFCD88 | grep docker@docker.com || exit 1
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# sudo apt-get install -y docker-ce
+# sudo docker run --rm hello-world
 service docker start
 
 docker ps
