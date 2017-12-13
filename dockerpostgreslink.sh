76a58b2649d67428e9923744ee3d27f56834b14d
Mithil Manjrekar <mithil.manjrekar@cuelogic.com>
2017-12-13 12:15:54 +0530

    trying ot run docker a super user

--- dockerpostgreslink.sh
+++ dockerpostgreslink.sh
@@ -14,33 +14,33 @@ su ${USER}
 
 # apt-cache madison docker-ee
 
-sudo docker run hello-world
+docker run hello-world
 
-sudo docker ps
+docker ps
 
 echo "Create a docker postgres image .........."
 
-sudo docker build -t postgres
+docker build -t postgres
 
 echo "Run the docker postgres image .........."
 
-sudo docker run --rm  postgres
+docker run --rm  postgres
 
 echo "Docker building the rails app form the Dockerfile .........."
 
-sudo docker build -t rails-app .
+docker build -t rails-app .
 
 echo "Docker running the rails app container created form the Dockerfile .........."
 
-sudo docker run  --rm -d --name rails-connect-to-potgres --link postgres:postgres -p 3000:3000 rails-app
+docker run  --rm -d --name rails-connect-to-potgres --link postgres:postgres -p 3000:3000 rails-app
 
 echo "Docker running the migrations on postgres .........."
 
-sudo docker exec rails-connect-to-potgres bundle exec rake db:create
-sudo docker exec rails-connect-to-potgres bundle exec rake db:migrate
+docker exec rails-connect-to-potgres bundle exec rake db:create
+docker exec rails-connect-to-potgres bundle exec rake db:migrate
 
 echo "Running the docker test cases .........."
 
-sudo docker exec rails-connect-to-potgres rspec spec/models/user_spec.rb 
+docker exec rails-connect-to-potgres rspec spec/models/user_spec.rb 
 
 echo "Docker ends testing here .........."
